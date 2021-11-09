0adcbcea521c273802103174525326659d316fe4
Nikolay <nikolay.vanzhin@yandex.ru>
2021-11-09 12:42:35 +0500

    view catalog update

--- views/product/catalog.php
+++ views/product/catalog.php
@@ -1,6 +1,6 @@
 <div id="main">
     <div class="post_title"><h2>Каталог</h2></div>
-    <div class="gallery">
+    <div class="gallery" id="gallery">
         <?php foreach($catalog as $item):?>
             <div class="gallery_item">
                 <a rel="gallery" class="photo" href="/product/card/?id=<?=$item['id']?>"><img alt="<?=$item['title']?>" src="gallery_img/small/<?=$item['title']?>" width="150" height="100" /></a>
@@ -18,6 +18,32 @@
     </div>
 </div>
 <script>
+/*поскольку карточки подгружаются по две за раз, остальные подгружаются по клику, надо навешивать события
+  на вновь отрисованные элементы: логика такая - как только отрисовываются еще карточки я навешиваю на них атрибут buy
+  , затем при повторном вызове функции buyClick, слушатель навешивается только на те, у которых атрибута нет*/
+function buyClick(){
+        const buttonsBuy = document.querySelectorAll('.buy');
+        buttonsBuy.forEach((elem)=>{
+            if(!elem.hasAttribute('buy')){
+                elem.setAttribute('buy', '1');
+                elem.addEventListener('click',() =>{
+                    const id = elem.getAttribute('buy-id');
+
+                    (async () =>{
+                            const response = await fetch("/cart/add/?id=" + id);
+                            const answer = await response.json();
+                            console.log(answer);
+                            if (answer.status === 'ok'){
+                                document.getElementById('cart_num').innerHTML = "( " + answer.total + " )";
+                            }
+                        }
+                    )()
+                })
+            }
+
+        });
+
+    }
     const more = document.getElementById('more');
     more.addEventListener('click',(e) =>{
         e.preventDefault();
@@ -28,54 +54,13 @@
                     const answer = await response.text();
                     const a = document.getElementsByClassName('gallery')[0];
                     a.insertAdjacentHTML('beforeend', answer);
+                    buyClick();
                 }
             )()
         });
-    //todo навесить события на вновь отрисованные элементы
-    // function on(node, event, className, cb) {
-    //     node.addEventListener(event, (e) => {
-    //         if (!e.target.classList.contains(className)) {
-    //             return false
-    //         }
-    //         cb(e)
-    //     })
-    // }
-    // const body = document.querySelector('body')
-    // on(body, 'click', 'buy', e => {
-    //     const elem = e.target;
-    //     console.log(elem);
-    //     elem.addEventListener('click',() =>{
-    //         const id = elem.getAttribute('buy-id');
-    //
-    //         (async () =>{
-    //                 const response = await fetch("/cart/add/?id=" + id);
-    //                 const answer = await response.json();
-    //                 console.log(answer);
-    //                 if (answer.status === 'ok'){
-    //                     document.getElementById('cart_num').innerHTML = "( " + answer.total + " )";
-    //                 }
-    //             }
-    //         )()
-    //     })
-    //
-    // })
 
-        const buttonsBuy = document.querySelectorAll('.buy');
-        buttonsBuy.forEach((elem)=>{
-        elem.addEventListener('click',() =>{
-            const id = elem.getAttribute('buy-id');
 
-            (async () =>{
-                    const response = await fetch("/cart/add/?id=" + id);
-                    const answer = await response.json();
-                    console.log(answer);
-                    if (answer.status === 'ok'){
-                        document.getElementById('cart_num').innerHTML = "( " + answer.total + " )";
-                    }
-                }
-            )()
-        })
-    });
+ buyClick();
 
 </script>
 
